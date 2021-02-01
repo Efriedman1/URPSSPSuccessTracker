ef386b6e90d5926aebc1d75907def668f4fa93c0
Anthony Nathan Littlejohn <tuh40168@temple.edu>
2021-02-01 13:37:05 -0500

    Replaced button and input field with an actual file upload control. Added regular expression validator to show error if wrong file type is uploaded

--- URPSSPSuccessTracker/AdminDownload.aspx.designer.cs
+++ URPSSPSuccessTracker/AdminDownload.aspx.designer.cs
@@ -76,6 +76,24 @@ namespace URPSSPSuccessTracker {
         protected global::System.Web.UI.WebControls.Label lblUpDirections;
         
         /// <summary>
+        /// fileUploadTemplate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload fileUploadTemplate;
+        
+        /// <summary>
+        /// RegularExpressionValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
+        
+        /// <summary>
         /// Table1 control.
         /// </summary>
         /// <remarks>
