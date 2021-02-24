42d8a041dc38f6074ab772c33fa746cc88eab66a
Anthony Nathan Littlejohn <tuh40168@temple.edu>
2021-02-24 11:15:54 -0500

    Added PrincipalInvestigator class Added changes to Student class Add chages to AdminDownloadPage

--- Classes/Student.cs
+++ Classes/Student.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using URPSSPSuccessTracker.Library;
 
 namespace URPSSPSuccessTracker.Classes
 {
     public class Student
     {
+
+        private WebService.StudentObj studentObj;
+        private string program;
+
+
+        //null Constructor
+        public Student()
+        {
+
+        }
+
+        public Student(WebService.StudentObj studentObj, string program)
+        {
+            this.studentObj = studentObj;
+            this.program = program;
+
+        }
+
+
+        public WebService.StudentObj StudentObj
+        {
+            get { return studentObj; }
+            set { studentObj = value; }
+        }
+
+        public string Program
+        {
+            get { return program; }
+            set { program = value; }
+        }
     }
 }
