ed195cfa1ee7b207be8a68bbb6b2c5cb3f3b9fc3
Anthony Nathan Littlejohn <tuh40168@temple.edu>
2021-03-04 17:33:10 -0500

    changes

--- secure/AdminDownload.aspx.cs
+++ secure/AdminDownload.aspx.cs
@@ -29,131 +29,125 @@ namespace URPSSPSuccessTracker
                 this.Master.SetNavBar((String)Session["UserType"]);
             }
 
-            
+
 
         }
 
         protected void btnUpload_Click(object sender, EventArgs e)
         {
-            ////Check if the file is an xls file
-            //if (fileUploadTemplate.HasFile)
-            //{
-            //    //if the file includes xls then use it
-            //    if (validateTemplate.IsValid)
-            //    {
+            //Check if the file is an xls file
+            if (fileUploadTemplate.HasFile)
+            {
+                //if the file includes xls then use it
+                if (validateTemplate.IsValid)
+                {
 
-            //        //create a new instance of the studentList to hold every student that was uplaoded from the template
-            //        studentList = new List<Student>();
-            //        failedUploads = new List<Student>();
-            //        Classes.Student student;
+                    //create a new instance of the studentList to hold every student that was uplaoded from the template
+                    studentList = new List<Student>();
+                    failedUploads = new List<Student>();
+                    Classes.Student student;
 
-            //        //the file uploaded is valid and begin reading file
-            //        //the file uploaded is valid and begin reading file
+                    //the file uploaded is valid and begin reading file
+                    //the file uploaded is valid and begin reading file
 
-            //        //read the uploaded excel file
-            //        IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(fileUploadTemplate.PostedFile.InputStream);
-            //        DataSet ds = excelReader.AsDataSet();
-            //        excelReader.Close();
+                    //read the uploaded excel file
+                    IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(fileUploadTemplate.PostedFile.InputStream);
+                    DataSet ds = excelReader.AsDataSet();
+                    excelReader.Close();
 
-            //        System.Data.DataTable dt = ds.Tables[0];
+                    System.Data.DataTable dt = ds.Tables[0];
 
-            //        //Create a list of strings, each string is a row of data
-            //        List<String> entryList = new List<string>();
+                    //Create a list of strings, each string is a row of data
+                    List<String> entryList = new List<string>();
 
-            //        int rowCount = dt.Rows.Count;
-            //        int columnCount = dt.Columns.Count;
-
-
-            //        foreach (DataRow dr in dt.Rows)
-            //        {
-            //            //if the student checkbox is selected, only get the tuid and student type
-            //            if (optStudent.Checked == true)
-            //            {
-            //                string entry = dr[0].ToString() + "," + dr[1].ToString();
-            //                entryList.Add(entry);
-
-            //            }
+                    int rowCount = dt.Rows.Count;
+                    int columnCount = dt.Columns.Count;
 
 
-            //            //else, that means the PI checkbox is selected, get the tuid and department
-            //            else if (optPI.Checked == true)
-            //            {
-            //                string entry = dr[0].ToString() + "," + dr[2].ToString();
-            //                entryList.Add(entry);
-            //            }
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        //if the student checkbox is selected, only get the tuid and student type
+                        if (optStudent.Checked == true)
+                        {
+                            string entry = dr[0].ToString() + "," + dr[1].ToString();
+                            entryList.Add(entry);
 
-                      
-            //        }
-
-            //        if (optStudent.Checked == true)
-            //        {
-            //            for (int i = 1; i < entryList.Count; i++)
-            //            {
-            //                string[] entryArray = entryList[i].Split(',');
-            //                WebService.StudentObj studentObj = WebService.Webservice.getStudentInfo(entryArray[0]);
-            //                string program = entryArray[1];
-
-            //                //create a student object and add the object from web services and the program
-            //                student = new Student(studentObj, program);
-
-            //                //add this new student object to a list of all students uploaded from the template
-            //                studentList.Add(student);
-
-            //                //upload to the database
-            //                //foreach (Student s in studentList)
-            //                //{
-
-            //                //    string tuid = s.StudentObj.tuid;
-            //                //    string firstName = s.StudentObj.firstName;
-            //                //    string lastName = s.StudentObj.lastName;
-            //                //    string email = s.StudentObj.email;
-            //                //    string program = s.Program;
-            //                //    string
-
-            //                //    bool addStudent = sql.AddStudent();
-
-            //                //    if (addStudent == false)
-            //                //    {
-            //                //        failedUploads.Add(s);
-            //                //    }
-            //                //}
-
-            //            }
-            //        }
-
-            //        else if (optPI.Checked == true)
-            //        {
-
-            //        }
-                   
-
-                   
-                   
+                        }
 
 
-            //    }
+                        //else, that means the PI checkbox is selected, get the tuid and department
+                        else if (optPI.Checked == true)
+                        {
+                            string entry = dr[0].ToString() + "," + dr[2].ToString();
+                            entryList.Add(entry);
+                        }
 
-            //    else
-            //    {
-            //        //the file is not valid/ show error message
-            //        lblError.Text = "Must upload a valid formatted '.xls' file";
-            //        lblError.Visible = true;
-            //    }
 
-            //}
+                    }
 
-            ////If no file was uploaded then put error message
-            //else
-            //{
-            //    lblError.Text = "Must upload a valid formatted '.xls' file";
-            //    lblError.Visible = true;
-            //}
+                    if (optStudent.Checked == true)
+                    {
+                        for (int i = 1; i < entryList.Count; i++)
+                        {
+                            string[] entryArray = entryList[i].Split(',');
+                            WebService.StudentObj studentObj = WebService.Webservice.getStudentInfo(entryArray[0]);
+                            string program = entryArray[1];
 
-        }
+                            //create a student object and add the object from web services and the program
+                            student = new Student(studentObj, program);
 
-        protected void btnDownload_Click(object sender, EventArgs e)
-        {
-           
+                            //add this new student object to a list of all students uploaded from the template
+                            studentList.Add(student);
+
+                            //upload to the database
+                            //foreach (Student s in studentList)
+                            //{
+
+                            //    string tuid = s.StudentObj.tuid;
+                            //    string firstName = s.StudentObj.firstName;
+                            //    string lastName = s.StudentObj.lastName;
+                            //    string email = s.StudentObj.email;
+                            //    string program = s.Program;
+                            //    string
+
+                            //    bool addStudent = sql.AddStudent();
+
+                            //    if (addStudent == false)
+                            //    {
+                            //        failedUploads.Add(s);
+                            //    }
+                            //}
+
+                        }
+                    }
+
+                    else if (optPI.Checked == true)
+                    {
+
+                    }
+
+
+
+
+
+
+                }
+
+                else
+                {
+                    //the file is not valid/ show error message
+                    lblError.Text = "Must upload a valid formatted '.xls' file";
+                    lblError.Visible = true;
+                }
+
+            }
+
+            //If no file was uploaded then put error message
+            else
+            {
+                lblError.Text = "Must upload a valid formatted '.xls' file";
+                lblError.Visible = true;
+            }
         }
     }
 }
