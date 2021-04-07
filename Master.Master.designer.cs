f054f91139f8d2fd1d23f6cec6b548afc1c0750c
Eric S Friedman <tug45244@temple.edu>
2021-04-07 13:15:30 -0400

    css edits

--- Master.Master.designer.cs
+++ Master.Master.designer.cs
@@ -85,6 +85,15 @@ namespace URPSSPSuccessTracker {
         protected global::System.Web.UI.WebControls.Button btnLogout;
         
         /// <summary>
+        /// DropDownList2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
