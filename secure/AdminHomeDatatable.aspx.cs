1ae30ee52becddf3fd531719de08985171d50cf0
Anthony Nathan Littlejohn <tuh40168@temple.edu>
2021-04-07 12:17:31 -0400

    Show correct name for comments, added datatable for PI, Changed data shown on tables, Show correct research project

--- secure/AdminHomeDatatable.aspx.cs
+++ secure/AdminHomeDatatable.aspx.cs
@@ -46,11 +46,34 @@ namespace URPSSPSuccessTracker
 
         protected void populateDataTable()
         {
-            DataSet studentData = procedures.LoadStudents();
+            DataSet studentData = procedures.LoadStudents("Fall", "2020");
+            DataSet piData = procedures.LoadPrincipalInvestigator("Fall", "2020");
             if (studentData.Tables.Count > 0)
             {
                 gvStudents.DataSource = studentData;
+
+                //add the research ID to the datakeys collection so that the correct
+                //research will be loaded on the view research page
+                string[] names = new string[1];
+                names[0] = "ResearchID";
+                gvStudents.DataKeyNames = names;
+
                 gvStudents.DataBind();
+
+            }
+
+            if (piData.Tables.Count > 0)
+            {
+                gvPI.DataSource = piData;
+
+                //add the research ID to the datakeys collection so that the correct
+                //research will be loaded on the view research page
+                //string[] names = new string[1];
+                //names[0] = "ResearchID";
+                //gvPI.DataKeyNames = names;
+
+                gvPI.DataBind();
+
             }
         }
 
@@ -172,6 +195,31 @@ namespace URPSSPSuccessTracker
             if (e.Row.RowType == DataControlRowType.Header)
             {
                 e.Row.TableSection = TableRowSection.TableHeader;
+            }
+        }
+
+        protected void gvStudents_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int rowIndex = Convert.ToInt32(e.CommandArgument.ToString());
+
+            if (e.CommandName == "View")
+            {
+                int researchID = int.Parse(gvStudents.DataKeys[rowIndex].Value.ToString());
+                Session.Add("researchID", researchID);
+                Response.Redirect("PIViewStudentResearch.aspx");
+            }
+
+        }
+
+        protected void gvPI_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int rowIndex = Convert.ToInt32(e.CommandArgument.ToString());
+
+            if (e.CommandName == "View")
+            {
+                int researchID = int.Parse(gvStudents.DataKeys[rowIndex].Value.ToString());
+                Session.Add("researchID", researchID);
+                Response.Redirect("PIViewStudentResearch.aspx");
             }
         }
     }
